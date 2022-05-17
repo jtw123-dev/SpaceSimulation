e20076bfc183862c2c6809336e3dfbe5b83eea41
jtw123-dev <78871610+jtw123-dev@users.noreply.github.com>
2022-05-17 10:24:30 -0400

    Finished UI and added screensaver

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -1,34 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CameraSwitch : MonoBehaviour
 {
     [SerializeField] GameObject _internalCamera;
     [SerializeField] GameObject _externalCamera;
+    [SerializeField] PlayableDirector _director;
     private bool _cameraSwitch;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]private float _timeToSwitch=5;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) &&_cameraSwitch ==false)
+        _timeToSwitch -= Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.R) && _cameraSwitch == false)
         {
             _cameraSwitch = true;
             _internalCamera.SetActive(false);
             _externalCamera.SetActive(true);
         }
-       else if (Input.GetKeyDown(KeyCode.R) && _cameraSwitch == true)
+        else if (Input.GetKeyDown(KeyCode.R) && _cameraSwitch == true)
         {
             _cameraSwitch = false;
             _internalCamera.SetActive(true);
             _externalCamera.SetActive(false);
         }
-    }
 
-    
+
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {          
+            _timeToSwitch = 5;
+            _director.Stop();
+           GameObject.Find("CameraVolume").SetActive(false);            
+        }     
+
+        if (Input.anyKeyDown ==false && _timeToSwitch <= 0  ) 
+        {         
+            _director.Play();        
+        }
+       
+       else if (Input.anyKeyDown ==true ||Input.anyKey==true)
+        {
+            _timeToSwitch = 5;
+            _director.Stop();      
+            GameObject.Find("CameraVolume").SetActive(false);        
+        }
+
+       
+    }       
 }
