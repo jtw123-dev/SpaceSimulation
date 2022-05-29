a6cba0ff458e5c5924fd5cbb97a2b368edea08eb
jtw123-dev <78871610+jtw123-dev@users.noreply.github.com>
2022-05-29 10:11:29 -0400

    Last upload before second build

--- Assets/Scripts/TriggerIngameCutscene.cs
+++ Assets/Scripts/TriggerIngameCutscene.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class TriggerIngameCutscene : MonoBehaviour
 {
     [SerializeField] private PlayableDirector _director;
+    [SerializeField] private bool _finalCutScene;
+
+    private void Update()
+    {
+        if (_finalCutScene == true && _director.time > 10)
+        {
+            Debug.Log("This is true");
+            _director.Pause();
+        }
+
+
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag =="Player")
@@ -14,4 +29,6 @@ public class TriggerIngameCutscene : MonoBehaviour
             _director.Play();
         }
     }
+
+    
 }
